e7ad2c079270c9a26cd0144a1a3ff4018cc7f52e
MWalsemann <walsemannmark@gmail.com>
2020-01-31 03:23:49 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+
+// Programmer name: Mark Walsemann
+// Date completed:  1/30/2020
+// Description: Output Part 1
+
+package main
+
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("My name is Mark, and my major is Computer Science.")
+	fmt.Println("This is my second semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is playing high school sports.")
+  
+}
