e279aa542ca030f508893898c383c731c34c9afd
MWalsemann <walsemannmark@gmail.com>
2020-01-31 03:27:37 +0000

    nothing

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ package main
 import "fmt" 
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello world !")
 	fmt.Println("My name is Mark, and my major is Computer Science.")
 	fmt.Println("This is my second semester at Butte College.")
 	fmt.Println("One of my favorite pastimes is playing high school sports.")
